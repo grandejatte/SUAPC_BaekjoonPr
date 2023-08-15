a76dddd9d313023ef5a968346d0e408519be1f17
grandejatte <141816316+grandejatte@users.noreply.github.com>
2023-08-15 16:29:02 +0900

    Add files via upload

--- 18_2884번.py
+++ 18_2884번.py
@@ -3,18 +3,19 @@ while not 0<=H<=23:
     H, M=map(int,input().split())
 while not 0<=M<=59:
     H, M=map(int,input().split())
-    
+
 if H != 0:
-    if M>45:
+    if M>=45:
         M-=45
-    elif M==45:
-        M=0
     else:
         H-=1
         M=60-(45-M)
 
-else:
-    H=23
-    M=60-(45-M)
-    
+elif H == 0:
+    if M>=45:
+        M-=45
+    else:
+        H=23
+        M=60-(45-M)
+        
 print(H, M)
